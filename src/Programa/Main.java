da39f909600b53e2f8e61770aa2b18bbc3c320fb
GabrielDBrito <britogabrielc@gmail.com>
2023-11-08 14:42:30 -0400

    package programa

---
+++ src/Programa/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Programa;
+
+/**
+ * @author Gabriel
+ * @version: 
+ */
+public class Main {
+    public static void main(String[] args){
+        App app = new App();
+        app.inicio();
+    }   
+}
+        
+            
+    
+
